9a9d31e59cb712363ff446374958bddb55fb5cba
HARSHIYASHAIK10 <harshiyashaik103@gmail.com>
2025-11-22 18:41:49 +0530

    Insert initial data for customers, products, and orders

---
+++ insert_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO customers VALUES
+(1, 'Harshiya', 'Hyderabad', '2024-01-10'),
+(2, 'Rahul', 'Bangalore', '2024-02-18'),
+(3, 'Fatima', 'Chennai', '2024-03-05');
+
+INSERT INTO products VALUES
+(101, 'Laptop', 'Electronics', 55000),
+(102, 'Headphones', 'Electronics', 2000),
+(103, 'T-shirt', 'Fashion', 500);
+
+INSERT INTO orders VALUES
+(1001, 1, 101, 1, '2024-04-10'),
+(1002, 1, 102, 2, '2024-04-12'),
+(1003, 2, 103, 5, '2024-04-15'),
+(1004, 3, 102, 1, '2024-04-16');
